c7e8924555f26e43f32e11cf38a101d52e690748
bear <xxrBear@gmail.com>
2025-04-27 19:42:23 +0800

    后端项目全局通用配置完成

--- src/main/java/cn/xxrbear/picturebackend/PictureBackendApplication.java
+++ src/main/java/cn/xxrbear/picturebackend/PictureBackendApplication.java
@@ -1,9 +1,13 @@
 package cn.xxrbear.picturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("cn.xxrbear.picturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class PictureBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/cn/xxrbear/picturebackend/controller/MainController.java
@@ -0,0 +1,21 @@
+package cn.xxrbear.picturebackend.controller;
+
+import cn.xxrbear.picturebackend.common.BaseResponse;
+import cn.xxrbear.picturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
+
